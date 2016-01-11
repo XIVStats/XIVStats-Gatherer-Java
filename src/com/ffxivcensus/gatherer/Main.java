d390e88539ba38a6c0dd9edc2be7078893cacd0c
Peter Reid <peter@reidweb.com>
2016-01-11 22:58:56 +0000

    Records now written to DB

--- src/com/ffxivcensus/gatherer/Main.java
+++ src/com/ffxivcensus/gatherer/Main.java
@@ -2,7 +2,7 @@ package com.ffxivcensus.gatherer;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Main class of character gathering program.
@@ -59,11 +60,33 @@ public class Main {
                     System.out.println(ex.getMessage());
                 }
 
+                //Create DB table if it doesn't exist
+                //Open connection
+                Connection conn = openConnection();
+                try {
+                    Statement st = conn.createStatement();
+                    String strSQL = "CREATE TABLE IF NOT EXISTS tblplayers (id INTEGER PRIMARY KEY,name TEXT,realm TEXT,race TEXT,gender TEXT,grand_company TEXT"
+                            + ",level_gladiator INTEGER,level_pugilist INTEGER,level_marauder INTEGER,level_lancer INTEGER,level_archer INTEGER"
+                            + ",level_rogue INTEGER,level_conjurer INTEGER,level_thaumaturge INTEGER,level_arcanist INTEGER,level_darkknight INTEGER, level_machinist INTEGER"
+                            + ",level_astrologian INTEGER,level_carpenter INTEGER,level_blacksmith INTEGER,level_armorer INTEGER,level_goldsmith INTEGER"
+                            + ",level_leatherworker INTEGER,level_weaver INTEGER,level_alchemist INTEGER,level_culinarian INTEGER,level_miner INTEGER"
+                            + ",level_botanist INTEGER,level_fisher INTEGER,p30days BIT, p60days BIT, p90days BIT, p180days BIT, p270days BIT"
+                            + ",p360days BIT,p450days BIT,p630days BIT,prearr BIT,prehw BIT, artbook BIT, beforemeteor BIT, beforethefall BIT"
+                            + ",soundtrack BIT,saweternalbond BIT,sightseeing BIT,arr_25_complete BIT,comm50 BIT,moogleplush BIT"
+                            + ",hildibrand BIT, ps4collectors BIT, dideternalbond BIT, arrcollector BIT, kobold BIT, sahagin BIT, amaljaa BIT, sylph BIT);";
+
+                    st.executeUpdate(strSQL);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                closeConnection(conn);
+
+
                 if (highestID < lowestID) {
                     System.out.println("Error: The second argument needs to be greater than the first argument");
                 } else { //Else pass values into poll method
                     //gatherRange(lowestID, highestID);
-                    Player.getPlayer(11886902);
+                    writeToDB(Player.getPlayer(11886902));
                 }
 
             } catch (Exception ex) {
@@ -82,22 +105,21 @@ public class Main {
      */
     public static void readConfig() throws ParserConfigurationException, IOException, SAXException {
         //Set config file location
-        File fXmlFile = new File("/config.xml");
+        File xmlFile = new File("config.xml");
         //Initialize parsers
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         //Parse the file
-        Document doc = dBuilder.parse(fXmlFile);
-        //Normalize the parsed value
-        doc.getDocumentElement().normalize();
+        Document doc = dBuilder.parse(xmlFile);
 
         //Read out config
-        Node dbNode = doc.getElementsByTagName("jdbc").item(0);
-        Element eElement = (Element) dbNode;
-        String url = eElement.getAttribute("url") + eElement.getAttribute("database");
+        NodeList nodes = doc.getElementsByTagName("jdbc");
+        Element eElement = (Element) nodes.item(0);
+
+        String url = eElement.getElementsByTagName("url").item(0).getTextContent() + eElement.getElementsByTagName("database").item(0).getTextContent();
         DB_URL = url;
-        DB_USER = eElement.getAttribute("username");
-        DB_PASSWORD = eElement.getAttribute("password");
+        DB_USER = eElement.getElementsByTagName("username").item(0).getTextContent();
+        DB_PASSWORD = eElement.getElementsByTagName("password").item(0).getTextContent();
     }
 
     /**
@@ -121,6 +143,30 @@ public class Main {
 
 
     public static void writeToDB(Player player) {
+        //Open connection
+        Connection conn = openConnection();
+        try {
+            //TODO Add hw_complete and hw_31_complete to table fields
+            Statement st = conn.createStatement();
+            String strSQL = "INSERT INTO tblplayers (id, name, realm, race, gender, grand_company, level_gladiator, level_pugilist, level_marauder,"
+                    + "level_lancer, level_archer, level_rogue, level_conjurer, level_thaumaturge, level_arcanist, level_astrologian, level_darkknight, level_machinist, level_carpenter,"
+                    + "level_blacksmith, level_armorer, level_goldsmith, level_leatherworker, level_weaver, level_alchemist,"
+                    + "level_culinarian, level_miner, level_botanist, level_fisher, p30days, p60days, p90days, p180days, p270days, p360days, p450days, p630days,"
+                    + "prearr, prehw, artbook, beforemeteor, beforethefall, soundtrack, saweternalbond, sightseeing, arr_25_complete, comm50, moogleplush,"
+                    + "hildibrand, ps4collectors, dideternalbond, arrcollector, kobold, sahagin, amaljaa, sylph) "
+                    + "VALUES(" + player.getId() + ",\"" + player.getPlayerName() + "\",\"" + player.getRealm() + "\",\"" + player.getRace() + "\",'" + player.getGender() + "','" + player.getGrandCompany() + "'," + player.getLvlGladiator() + "," + player.getLvlPugilist() + "," + player.getLvlMarauder() + ","
+                    + player.getLvlLancer() + "," + player.getLvlArcher() + "," + player.getLvlRogue() + "," + player.getLvlConjurer() + "," + player.getLvlThaumaturge() + "," + player.getLvlArcanist() + "," + player.getLvlAstrologian() + "," + player.getLvlDarkKnight() + "," + player.getLvlMachinist() + "," + player.getLvlCarpenter() + ","
+                    + player.getLvlBlacksmith() + "," + player.getLvlArmorer() + "," + player.getLvlGoldsmith() + "," + player.getLvlLeatherworker() + "," + player.getLvlWeaver() + "," + player.getLvlAlchemist() + ","
+                    + player.getLvlCulinarian() + "," + player.getLvlMiner() + "," + player.getLvlBotanist() + "," + player.getLvlFisher() + "," + player.getBitHas30DaysSub() + "," + player.getBitHas60DaysSub() + "," + player.getBitHas90DaysSub() + "," + player.getBitHas180DaysSub() + "," + player.getBitHas270DaysSub() + "," + player.getBitHas360DaysSub() + "," + player.getBitHas450DaysSub() + "," + player.getBitHas630DaysSub() + ","
+                    + player.getBitHasPreOrderArr() + "," + player.getBitHasPreOrderHW() + "," + player.getBitHasArtBook() + "," + player.getBitHasBeforeMeteor() + "," + player.getBitHasBeforeTheFall() + "," + player.getBitHasSoundTrack() + "," + player.getBitHasAttendedEternalBond() + "," + player.getBitHasCompletedHWSightseeing() + "," + player.getBitHasCompleted2pt5() + "," + player.getBitHasFiftyComms() + "," + player.getBitHasMooglePlush() + ","
+                    + player.getBitHasCompletedHildibrand() + "," + player.getBitHasPS4Collectors() + "," + player.getBitHasEternalBond() + "," + player.getBitHasARRCollectors() + "," + player.getBitHasKobold() + "," + player.getBitHasSahagin() + "," + player.getBitHasAmaljaa() + "," + player.getBitHasSylph()
+                    + ");";
+
+            st.executeUpdate(strSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     /**
@@ -129,14 +175,14 @@ public class Main {
      * @return the opened connection
      * @throws SQLException exception thrown if unable to connect
      */
-    private static Connection openConnection() throws SQLException {
+    private static Connection openConnection() {
         try {
             Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
             return connection;
         } catch (SQLException sqlEx) {
             System.out.println("Connection failed! Please see output console");
             sqlEx.printStackTrace();
-            throw new SQLException(sqlEx);
+            return null;
         }
     }
 
