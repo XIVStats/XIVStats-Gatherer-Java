3d5da733b728540449e1fed563e5d9c7e1ed8c86
Jonathan Price <jonathan@jonathanprice.uk>
2016-07-26 20:39:26 +0100

    Resolved several occurrences of hardcoded hostname values for database connections, causing JUnit tests to fail. The core project was fine but JUnit tests were failing if the database was not on localhost.
    Many, many whitespace "improvements"

--- src/test/java/com/ffxivcensus/gatherer/ConsoleTest.java
+++ src/test/java/com/ffxivcensus/gatherer/ConsoleTest.java
@@ -44,6 +44,10 @@ public class ConsoleTest {
      * The database to use
      */
     private static String dbName;
+    /**
+     * Hostname of database, e.g. mysql://host.example.com:3306
+     */
+    private static String dbHost;
 
     /**
      * Before running drop the table.
@@ -100,21 +104,22 @@ public class ConsoleTest {
     /**
      * Make a run of the console application with the following options.
      * <ul>
-     *     <li>Start ID = 0</li>
-     *     <li>End ID = 100</li>
-     *     <li>Thread limit = 10</li>
-     *     <li>Table Name = tblplayers_test</li>
-     *     <li>Log mounts</li>
-     *     <li>Log minions</li>
-     *     <li>Do not store progress indicators</li>
-     *     <li>User specified DB name.</li>
+     * <li>Start ID = 0</li>
+     * <li>End ID = 100</li>
+     * <li>Thread limit = 10</li>
+     * <li>Table Name = tblplayers_test</li>
+     * <li>Log mounts</li>
+     * <li>Log minions</li>
+     * <li>Do not store progress indicators</li>
+     * <li>User specified DB name.</li>
      * </ul>
+     *
      * @throws Exception
      */
     @Test
     public void testConsole() throws Exception {
 
-        String[] args = {"-s", "0", "-f", "100", "-t", "10","-T","tblplayers_test","-d",dbName,"-bPq"};
+        String[] args = {"-s", "0", "-f", "100", "-t", "10", "-T", "tblplayers_test", "-d", dbName,"-U",dbHost, "-bPq"};
 
         GathererController gc = Console.run(args);
         //Test that options have set attributes correctly
@@ -122,10 +127,10 @@ public class ConsoleTest {
         assertTrue(gc.isStoreMinions()); //P
         assertTrue(gc.isQuiet()); //q
         assertFalse(gc.isVerbose()); //q
-        assertEquals("tblplayers_test",gc.getTableName());
-        assertEquals(gc.getStartId(),0);
-        assertEquals(gc.getEndId(),100);
-        assertEquals(gc.getThreadLimit(),10);
+        assertEquals("tblplayers_test", gc.getTableName());
+        assertEquals(gc.getStartId(), 0);
+        assertEquals(gc.getEndId(), 100);
+        assertEquals(gc.getThreadLimit(), 10);
 
         assertTrue(outContent.toString().contains("Starting parse of range 0 to 100 using 10 threads"));
         assertTrue(outContent.toString().contains("Run completed, 101 character IDs scanned"));
@@ -136,31 +141,32 @@ public class ConsoleTest {
     /**
      * Make a run of the console application with the following options.
      * <ul>
-     *     <li>Start ID = 100</li>
-     *     <li>End ID = 200</li>
-     *     <li>Thread limit = 32</li>
-     *     <li>Print duds</li>
-     *     <li>User specified db name</li>
-     *     <li>User specified db credentials</li>
-     *     <li>Verbose mode</li>
+     * <li>Start ID = 100</li>
+     * <li>End ID = 200</li>
+     * <li>Thread limit = 32</li>
+     * <li>Print duds</li>
+     * <li>User specified db name</li>
+     * <li>User specified db credentials</li>
+     * <li>Verbose mode</li>
      * </ul>
+     *
      * @throws Exception
      */
     @Test
     public void testConsoleFullOptions() throws Exception {
 
-        String[] args = {"--start=100", "--finish=200", "--threads", "32","-v","-d",dbName,"-U","mysql://localhost:3306","-u",dbUser,"-p",dbPassword};
+        String[] args = {"--start=100", "--finish=200", "--threads", "32", "-v", "-d", dbName, "-U", dbHost, "-u", dbUser, "-p", dbPassword};
         GathererController gc = Console.run(args);
 
-        assertEquals(gc.getStartId(),100);
-        assertEquals(gc.getEndId(),200);
-        assertEquals(gc.getThreadLimit(),32);
+        assertEquals(gc.getStartId(), 100);
+        assertEquals(gc.getEndId(), 200);
+        assertEquals(gc.getThreadLimit(), 32);
         assertTrue(gc.isVerbose());
     }
 
 
     @Test
-    public void TestConsoleHelpDefault() throws Exception{
+    public void TestConsoleHelpDefault() throws Exception {
 
         String strHelp = "usage: java -jar XIVStats-Gatherer-Java.jar [-bmqvxFPS] -s startid -f";
 
@@ -172,7 +178,7 @@ public class ConsoleTest {
     }
 
     @Test
-    public void TestConsoleHelpOnFail() throws Exception{
+    public void TestConsoleHelpOnFail() throws Exception {
 
         String strHelp = "usage: java -jar XIVStats-Gatherer-Java.jar [-bmqvxFPS] -s startid -f";
         //Test for a help dialog displayed upon failure
@@ -184,7 +190,7 @@ public class ConsoleTest {
     }
 
     @Test
-    public void TestConsoleHelp() throws Exception{
+    public void TestConsoleHelp() throws Exception {
 
         String strHelp = "usage: java -jar XIVStats-Gatherer-Java.jar [-bmqvxFPS] -s startid -f";
 
@@ -196,24 +202,24 @@ public class ConsoleTest {
     }
 
     @Test
-    public void testMain(){
-        String[] args = {"-s","1100", "-f","1400"};
+    public void testMain() {
+        String[] args = {"-s", "1100", "-f", "1400"};
         Console.main(args);
         //Check output
         assertFalse(outContent.toString().contains("does not exist."));
     }
 
     @Test
-    public void testDefault(){
-        String[] args = {"-s","500","-f","600"};
+    public void testDefault() {
+        String[] args = {"-s", "500", "-f", "600"};
         GathererController gc = Console.run(args);
         //Check output
         assertFalse(outContent.toString().contains("does not exist."));
     }
 
     @Test
-    public void testPrintFails(){
-        String[] args = {"-s","700","-f","800","-qF"};
+    public void testPrintFails() {
+        String[] args = {"-s", "700", "-f", "800", "-qF"};
         GathererController gc = Console.run(args);
         //Check output
         assertFalse(outContent.toString().contains("written to database successfully."));
@@ -221,11 +227,11 @@ public class ConsoleTest {
     }
 
     @Test
-    public void testSplitTables(){
-        String[] args = {"-s","900","-f","1000","-x","_test","-S"};
+    public void testSplitTables() {
+        String[] args = {"-s", "900", "-f", "1000", "-x", "_test", "-S"};
         GathererController gc = Console.run(args);
         assertTrue(gc.isSplitTables());
-        assertEquals(gc.getTableSuffix(),"_test");
+        assertEquals(gc.getTableSuffix(), "_test");
     }
 
     //Utility methods
@@ -283,6 +289,7 @@ public class ConsoleTest {
 
         String url = "jdbc:" + elementJDBC.getElementsByTagName("url").item(0).getTextContent() + "/" + elementJDBC.getElementsByTagName("database").item(0).getTextContent();
         dbUrl = url;
+        dbHost = elementJDBC.getElementsByTagName("url").item(0).getTextContent();
         dbUser = elementJDBC.getElementsByTagName("username").item(0).getTextContent();
         dbPassword = elementJDBC.getElementsByTagName("password").item(0).getTextContent();
         dbName = elementJDBC.getElementsByTagName("database").item(0).getTextContent();
