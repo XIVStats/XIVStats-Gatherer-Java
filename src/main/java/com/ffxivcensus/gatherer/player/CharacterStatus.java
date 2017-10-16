61bcc53ffd95b6218899368cbf89a8a92bd79cbe
Matthew Hillier <majiknet@yahoo.co.uk>
2017-10-16 23:19:14 +0100

    Updates Parsing Error Handling
    Handle errors using the appropriate HttpStatusException class.
    Introduced basic CharacterStatus enum to track outcome from parse and decide how to use it once parse complete.

---
+++ src/main/java/com/ffxivcensus/gatherer/player/CharacterStatus.java
@@ -0,0 +1,14 @@
+package com.ffxivcensus.gatherer.player;
+
+/**
+ * Enumerates the currently identified Character Statuses.
+ * @author matthew.hillier
+ */
+public enum CharacterStatus {
+	/** Active Character. */
+	ACTIVE,
+	/** Inactive Character. */
+	INACTIVE,
+	/** Deleted Character. */
+	DELETED
+}
