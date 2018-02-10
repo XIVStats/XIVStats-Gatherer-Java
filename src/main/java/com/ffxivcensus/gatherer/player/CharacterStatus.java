1036aa08ade55930168af3474f24285529a13002
Matthew Hillier <majiknet@yahoo.co.uk>
2018-02-10 12:38:27 +0800

    Added storing the Character Status attribute.

--- src/main/java/com/ffxivcensus/gatherer/player/CharacterStatus.java
+++ src/main/java/com/ffxivcensus/gatherer/player/CharacterStatus.java
@@ -2,13 +2,14 @@ package com.ffxivcensus.gatherer.player;
 
 /**
  * Enumerates the currently identified Character Statuses.
+ * 
  * @author matthew.hillier
  */
 public enum CharacterStatus {
-	/** Active Character. */
-	ACTIVE,
-	/** Inactive Character. */
-	INACTIVE,
-	/** Deleted Character. */
-	DELETED
+    /** Active Character. */
+    ACTIVE,
+    /** Inactive Character. */
+    INACTIVE,
+    /** Deleted Character. */
+    DELETED
 }
