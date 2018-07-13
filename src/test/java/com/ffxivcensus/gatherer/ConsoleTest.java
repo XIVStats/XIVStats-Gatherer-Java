c65a2bba1006730d780f796ea3cbe366cec83cf0
Matthew Hillier <majiknet@yahoo.co.uk>
2018-07-13 18:51:58 +0800

    Additional coverage for safe execution of Help Docs

--- src/test/java/com/ffxivcensus/gatherer/ConsoleTest.java
+++ src/test/java/com/ffxivcensus/gatherer/ConsoleTest.java
@@ -1,18 +1,27 @@
 package com.ffxivcensus.gatherer;
 
+import static org.junit.Assert.fail;
+
+import org.apache.commons.cli.ParseException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class ConsoleTest {
-    
+
     private Console instance;
-    
+    @Mock
+    private GathererController controllerMock;
+
     @Before
     public void setup() {
         instance = new Console();
+        MockitoAnnotations.initMocks(this);
     }
-    
+
     @After
     public void tearDown() {
         instance = null;
@@ -20,6 +29,13 @@ public class ConsoleTest {
 
     @Test
     public void testRunWithoutController() {
+        instance.run((String[]) null);
+    }
+
+    @Test
+    public void testRunWithParseException() throws Exception {
+        Mockito.doThrow(ParseException.class).when(controllerMock).run();
+        instance.setGathererController(controllerMock);
         instance.run((String[]) null);
     }
 
